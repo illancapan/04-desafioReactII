67aed017149e9da00d226036e5474723f9f87060
illancapan <igorllancapan@gmail.com>
2024-04-23 21:02:08 -0400

    Pruebas

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <><h1> naaav</h1>
+<nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default NavBar
--- src/views/PantallaPrueba.jsx
+++ src/views/PantallaPrueba.jsx
@@ -1,11 +1,14 @@
 import { useContext } from 'react';
 import { ApiContext } from '../context/ApiContext';
+import NavBar from '../components/navBar/NavBar';
 
 const PantallaPrueba = () => {
   const pizzas = useContext(ApiContext);
 
   return (
     <div>
+<NavBar/>
+        
       <h1>Pantalla Prueba</h1>
       <div>
         {pizzas.map((pizza, index) => (
