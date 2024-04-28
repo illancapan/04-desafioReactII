b49afdfedd823e6e748e3bf0c63daa7408ee4763
illancapan <igorllancapan@gmail.com>
2024-04-28 17:40:21 -0400

    cambios

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
 
   // Calcular la cantidad total de pizzas en el carrito
   const totalPizzas = carro.reduce((total, pizza) => {
-    // Asegurarse de que tanto price como quantity sean números válidos
+    // aseguro tanto price como quantity sean numeros validos
     const pizzaPrice = typeof pizza.price === 'number' ? pizza.price : 0;
     const pizzaQuantity = typeof pizza.quantity === 'number' ? pizza.quantity : 0;
     // Sumar el precio de la pizza multiplicado por la cantidad
